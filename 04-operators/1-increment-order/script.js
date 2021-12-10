982ecf978b61edc11e1efb50c8b5ba0a7442a24c
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-10 09:49:37 +0100

    04-operators exercice done

--- 04-operators/1-increment-order/script.js
+++ 04-operators/1-increment-order/script.js
@@ -1,4 +1,6 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+"use strict";
+
+let a = 1, b = 1;
+
+let c = ++a; // 2
+let d = b++; // 1

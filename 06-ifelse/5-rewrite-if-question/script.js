6f3a5334305a00caba2ac00bf526222b5b87812d
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-12 18:01:09 +0100

    06-ifelse 5-rewrite-if-question exercise done

--- 06-ifelse/5-rewrite-if-question/script.js
+++ 06-ifelse/5-rewrite-if-question/script.js
@@ -1,4 +1,4 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let result;
+
+
+if(a+ b < 4) ? result = "Below" ? result = "Over";

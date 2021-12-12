64ededd5257c0feeffc84692c012d9a2dc1742b1
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-12 22:52:55 +0100

    12-while-for 5-replace-for-while exercise done

--- 12-while-for/5-replace-for-while/script.js
+++ 12-while-for/5-replace-for-while/script.js
@@ -1,4 +1,8 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+"use strict";
+
+let i = 0;
+
+while (i < 3) {
+      alert(`Number ${i} !`);
+      i++;
+}

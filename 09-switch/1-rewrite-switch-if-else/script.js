591b71f29a3526d8c2ac4ac64adabe601f65086e
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-12 23:19:36 +0100

    09-switch 1-rewrite-switch-if-else

--- 09-switch/1-rewrite-switch-if-else/script.js
+++ 09-switch/1-rewrite-switch-if-else/script.js
@@ -1,4 +1,11 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+"use strict";
+
+if (browser == 'Edge') {
+      alert("You've got the Edge!");
+
+} else if (browser == "Chrome" || browser == "Firefox" || brower == "Safari" || browser == "Opera") {
+      alert('Okay we support these browsers too');
+
+} else {
+      alert('We hope that this page looks ok!');
+}

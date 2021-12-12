a1d8699b724821d0c8c7b57cad7fa3058404f08f
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-12 22:43:11 +0100

    12-while-for 4-for-even exercise done

--- 12-while-for/4-for-even/script.js
+++ 12-while-for/4-for-even/script.js
@@ -1,4 +1,7 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+"use strict";
+
+for(let i = 2; i <= 10; i++) {
+    if(i % 2 == 0) {
+      alert(i);
+    }
+}

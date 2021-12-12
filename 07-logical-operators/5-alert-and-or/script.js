a453dcbd7d785c7930dff0193df3ed95157df55a
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-12 18:24:31 +0100

    07-logical 05-alert-and-or exercise done

--- 07-logical-operators/5-alert-and-or/script.js
+++ 07-logical-operators/5-alert-and-or/script.js
@@ -1,4 +1,2 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+// This will show an alert "3".
+alert( null || 2 && 3 || 4 );

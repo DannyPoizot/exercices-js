26933464c28d40823ab50548ca67fdc893213efa
Danny Poizot <dannypoizot@pop-os.localdomain>
2021-12-12 23:26:40 +0100

    12-while-for 3-which-value-for exercise done

--- 12-while-for/3-which-value-for/script.js
+++ 12-while-for/3-which-value-for/script.js
@@ -1,4 +1,5 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+for (let i = 0; i < 5; i++) alert( i );
+// 0, 1, 2, 3, 4, 5
+
+for (let i = 0; i < 5; ++i) alert( i );
+// 0, 1, 2, 3, 4, 5
